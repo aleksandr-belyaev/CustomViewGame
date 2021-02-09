5459a15d14b754ccf1f32954eedd5ead60cf658d
aleksandr-belyaev <belyaev.photo@gmail.com>
2021-02-09 08:12:08 +0700

    Initial Commit

---
+++ CustomViewGame/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CustomViewGame
+//
+//  Created by Александр Беляев on 09.02.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
