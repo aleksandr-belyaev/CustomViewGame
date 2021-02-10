33265f8a54212720d53b867e547f814f5dcf7720
aleksandr-belyaev <belyaev.photo@gmail.com>
2021-02-10 15:29:12 +0700

    gesture is working! But only on the last view

--- CustomViewGame/ViewController.swift
+++ CustomViewGame/ViewController.swift
@@ -10,7 +10,6 @@ import UIKit
 class ViewController: UIViewController, UIGestureRecognizerDelegate {
 
     var customView: CustomView!
-    var panGesture = UIPanGestureRecognizer()
     let viewCount: Int = 7
     
     override func viewDidLoad() {
@@ -19,11 +18,13 @@ class ViewController: UIViewController, UIGestureRecognizerDelegate {
         let viewSize = min(view.bounds.height, view.bounds.width)/4.0
         var x: CGFloat = viewSize/2
         var y: CGFloat = viewSize/2
+        let panGesture = UIPanGestureRecognizer(target: self, action: #selector(panAction))
+        panGesture.delegate = self
         
         for i in 0..<viewCount {
             customView = CustomView(frame: CGRect(x: x, y: y, width: viewSize, height: viewSize), number: String(i+1))
+            customView.addGestureRecognizer(panGesture)
             view.addSubview(customView)
-            
             if ((x + viewSize * 1.5) >= view.bounds.maxX) {
                 x = viewSize/2
                 y = y + viewSize + 5
@@ -31,44 +32,11 @@ class ViewController: UIViewController, UIGestureRecognizerDelegate {
                 x = x + viewSize + 5
             }
         }
-        customView.addGestureRecognizer(panGesture)
     }
     
-    
-    @objc func handlePanGesture() {
-        
+    @objc func panAction(_ gesture: UIPanGestureRecognizer) {
+        let vieww = gesture.view as! CustomView
+        print(vieww.elementNumberLabel.text!)
     }
-
-    @IBAction func panAction(_ gesture: UIPanGestureRecognizer) {
-        let firstViewFrame = customView.frame
-        let secondViewFrame = customView.frame
-        
-        let gestureTranslation = gesture.translation(in: view)
-        
-        guard let gestureView = gesture.view else {
-            return
-        }
-        
-        gestureView.center = CGPoint (
-            x: gestureView.center.x + gestureTranslation.x,
-            y: gestureView.center.y + gestureTranslation.y
-        )
-        
-        gesture.setTranslation(.zero, in: view)
-        
-        guard gesture.state == .ended else {
-            return
-        }
-        
-        print("Blue view panned")
-        
-        for value in Int(secondViewFrame.minY)...Int(secondViewFrame.maxY) {
-            if Int(firstViewFrame.origin.y) == value {
-                customView.isHidden = true
-            }
-        }
-        
-    }
-    
 }
 
